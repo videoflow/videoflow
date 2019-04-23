10dabe34b31299a3d6c3e93dcd437a9eaaedb1ce
Jadiel deArmas <jadielam@gmail.com>
2019-04-23 19:09:16 -0400

    Finished documenting producers

--- videoflow/producers/basic.py
+++ videoflow/producers/basic.py
@@ -7,6 +7,13 @@ import time
 from ..core.node import ProducerNode
 
 class IntProducer(ProducerNode):
+    '''
+    Each time the ``next`` method is called, produces the next
+    integer in the sequence between ``start_value`` and ``end_value``.
+    
+    If ``wait_time_in_seconds`` is greater than zero, it sleeps for 
+    the specified amount of seconds each time ``next()`` is called
+    '''
     def __init__(self, start_value : int = 0, end_value : int = None, 
                 wait_time_in_seconds : float = 0):
         self._start_value = start_value
@@ -16,6 +23,10 @@ class IntProducer(ProducerNode):
         super(IntProducer, self).__init__()
         
     def next(self):
+        '''
+        - Returns:
+            - next: an integer
+        '''
         if self._end_value is not None and self._current_value > self._end_value:
             raise StopIteration()
         to_return = self._current_value
--- videoflow/producers/video.py
+++ videoflow/producers/video.py
@@ -7,18 +7,27 @@ import cv2
 from ..core.node import ProducerNode
 
 class VideofileReader(ProducerNode):
+    '''
+    Opens a video capture object and returns subsequent frames
+    from the video each time ``next`` is called
+    '''
     def __init__(self, video_file : str):
         '''
-        Arguments:
-        - video_file: path to video file
-        - retry_attempts: number of retries to read video
-          stream before raising StopIteration exception
+        - Arguments:
+            - video_file: path to video file
         '''
         self._video_file = video_file
         self._video = None
         super(VideofileReader, self).__init__()
 
     def next(self):
+        '''
+        - Returns:
+            - frame: np.array of shape (h, w, 3)
+        
+        - Raises:
+            - StopIteration: after it finishes reading the videofile.
+        '''
         if self._video is None:
             self._video = cv2.VideoCapture(self._video_file)
 
