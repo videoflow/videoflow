69460dd6fd4c4bef7174c137a84d0b3dd9c5de8a
Jadiel deArmas <jadielam@gmail.com>
2019-04-23 18:53:20 -0400

    Finished the documentation of the basic processors

--- videoflow/processors/basic.py
+++ videoflow/processors/basic.py
@@ -5,9 +5,18 @@ from __future__ import absolute_import
 from ..core.node import ProcessorNode
 
 class IdentityProcessor(ProcessorNode):
+    '''
+    IdentityProcessor implements the identity
+    function: it returns the same value that it received
+    as input.
+    '''
     def process(self, inp):
         return inp
 
 class JoinerProcessor(ProcessorNode):
+    '''
+    Takes all the parameters received in the ``process`` method
+    and makes them a tuple of items.
+    '''
     def process(self, *inp):
         return tuple(inp)
