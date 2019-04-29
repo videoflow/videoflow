1f632862819a517b76d4a7439915795f0966f526
Jadiel deArmas <jadielam@gmail.com>
2019-04-29 11:20:09 -0400

    Modified video producer to use the ContextNode interface

--- videoflow/producers/video.py
+++ videoflow/producers/video.py
@@ -4,9 +4,9 @@ from __future__ import absolute_import
 
 import cv2
 
-from ..core.node import ProducerNode
+from ..core.node import ProducerNode, ContextNode
 
-class VideofileReader(ProducerNode):
+class VideofileReader(ProducerNode, ContextNode):
     '''
     Opens a video capture object and returns subsequent frames
     from the video each time ``next`` is called
@@ -19,6 +19,22 @@ class VideofileReader(ProducerNode):
         self._video_file = video_file
         self._video = None
         super(VideofileReader, self).__init__()
+    
+    def __enter__(self):
+        '''
+        Opens the video stream
+        '''
+        if self._video is None:
+            self._video = cv2.VideoCapture(self._video_file)
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        '''
+        Releases the video stream object
+        '''
+        self._video.release()
+        if exc_type is not None:
+            return False
+        return True
 
     def next(self):
         '''
@@ -28,9 +44,7 @@ class VideofileReader(ProducerNode):
         - Raises:
             - StopIteration: after it finishes reading the videofile.
         '''
-        if self._video is None:
-            self._video = cv2.VideoCapture(self._video_file)
-
+        
         if self._video.isOpened():
             success, frame = self._video.read()
             if not success:
@@ -39,5 +53,4 @@ class VideofileReader(ProducerNode):
             else:
                 return frame
         else:
-            self._video.release()
             raise StopIteration()
