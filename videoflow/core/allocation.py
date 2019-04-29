393aebe3d8835e6b9d5615c2649c88de08bf7060
Jadiel deArmas <jadielam@gmail.com>
2019-04-29 11:24:26 -0400

    defined allocatable and allocation classes

---
+++ videoflow/core/allocation.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+class Allocatable(object):
+    pass
+
+class Allocation(object):
+    pass
