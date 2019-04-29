f44a50402eab02f8285e872c29e836089bc21129
Jadiel deArmas <jadielam@gmail.com>
2019-04-29 11:22:03 -0400

    Created blueprint for VideofileWriter consumer

--- videoflow/consumers/video.py
+++ videoflow/consumers/video.py
@@ -4,10 +4,20 @@ from __future__ import absolute_import
 
 import cv2
 
-from ..core.node import ConsumerNode
+from ..core.node import ConsumerNode, ContextNode
 
-class VideofileWriter(ConsumerNode):
+class VideofileWriter(ConsumerNode, ContextNode):
     '''
     Opens a video file writer object and writes subsequent
     frames received into the object.
     '''
+    pass
+
+    def __enter__(self):
+        pass
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        pass
+
+    def consume(self, item):
+        pass
