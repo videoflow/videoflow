8a05a5b962c4b4af134651c40c7f3a68acca6f70
cclauss <cclauss@me.com>
2019-05-25 20:54:59 +0200

    Undefined name: from contextlib import closing
    https://docs.python.org/3/library/contextlib.html#contextlib.closing

--- videoflow/utils/downloader.py
+++ videoflow/utils/downloader.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+from contextlib import closing
 import sys
 import hashlib
 import os
