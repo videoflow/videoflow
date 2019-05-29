cbf613d81049f457ea70cb4b959e857104e4ba7e
Jadiel deArmas <jadielam@gmail.com>
2019-05-29 18:44:19 -0400

    Added function get available gpus

--- videoflow/utils/system.py
+++ videoflow/utils/system.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 def get_number_of_gpus() -> int:
     '''
@@ -10,6 +11,13 @@ def get_number_of_gpus() -> int:
     except FileNotFoundError:
         return 0
 
+def get_system_gpus() -> set:
+    '''
+    Returns the ids of gpus in the machine as a set of integers
+    '''
+    n = get_number_of_gpus()
+    return set(range(n))
+
 def get_available_gpus() -> [int]:
     '''
     Returns the list of ids of the gpus available to the process calling the function.
@@ -17,6 +25,24 @@ def get_available_gpus() -> [int]:
     available by ``CUDA_VISIBLE_DEVICES``. It returns the intersection of those
     two sets as a list.
     '''
-    # Remember that an empty ``CUDA_VISIBLE_DEVICES`` means that all devices are visible.
-    pass
+    system_devices = get_system_gpus()
+    env_var = os.environ.get('CUDA_VISIBLE_DEVICES', None)
+    if env_var is None:
+        visible_devices = set(system_devices)
+    else:
+        env_var = env_var.strip()
+        visible_devices = set()
+        if len(env_var) > 0:
+            devices = env_var.split(',')
+            for device in devices:
+                try:
+                    device_id = int(device)
+                    visible_devices.add(device_id)
+                except:
+                    pass
+    
+    available_devices = system_devices & visible_devices
+    return list(available_devices)
+
+
     
