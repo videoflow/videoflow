e2965960f99113e8eef91ff9bb45348824ff172e
Jadiel deArmas <jadielam@gmail.com>
2019-06-17 19:07:22 -0700

    Added util function to get masks from model

--- videoflow/processors/vision/segmentation.py
+++ videoflow/processors/vision/segmentation.py
@@ -13,6 +13,57 @@ import tensorflow as tf
 
 BASE_URL_SEGMENTATION = 'https://github.com/videoflow/videoflow/releases/download/segmentation/'
 
+def reframe_box_masks_to_image_masks(box_masks, boxes, h, w):
+    '''
+    Transforms the box masks back to full image masks
+
+    - Arguments:
+        - box_masks: tf.float32 tensor of size [nb_masks, height, width],
+        - boxes: tf.float32 tensor of size [nb_masks, 4] containing box \
+            coordinates [ymin, xmin, ymax, xmax].  Note that the coordinates \
+            are normalized
+        - h: Image height. The output masks will have height h.
+        - w: Image width. The output masks will have widht w.
+    
+    - Returns:
+        - image_masks: tf.float32 tnesor of size [nb_masks, h, w]
+    '''
+    def reframe_box_masks_to_image_masks_default():
+        '''
+        The default function when there are more than 0 box masks
+        '''
+        def transform_boxes_relative_to_boxes(boxes, reference_boxes):
+            boxes = tf.reshape(boxes, [-1, 2, 2])
+            min_corner = tf.expand_dims(reference_boxes[:, 0:2], 1)
+            max_corner = tf.expand_dims(reference_boxes[:, 2:4], 1)
+            transformed_boxes = (boxes - min_corner) / (max_corner - min_corner)
+            return tf.reshape(transformed_boxes, [-1, 4])
+        
+        box_masks_expanded = tf.expand_dims(box_masks, axis = 3)
+        num_boxes = tf.shape(box_masks_expanded)[0]
+        unit_boxes = tf.concat(
+            [
+                tf.zeros([num_boxes, 2]),
+                tf.ones([num_boxes, 2])
+            ],
+            axis = 1
+        )
+        reverse_boxes = transform_boxes_relative_to_boxes(unit_boxes, boxes)
+        return tf.image.crop_and_resize(
+            image = box_masks_expanded,
+            boxes = reverse_boxes,
+            box_ind = tf.range(num_boxes),
+            crop_size = [h, w],
+            extrapolation_value = 0.0
+        )
+    
+    image_masks = tf.cond(
+        tf.shape(box_masks)[0] > 0,
+        reframe_box_masks_to_image_masks_default,
+        lambda: tf.zeros([0, h, w, 1], dtype = tf.float32)
+    )
+    return tf.squeeze(image_masks, axis = 3)
+
 class Segmenter(ProcessorNode):
     '''
     Abstract class that defines the interface to do image
@@ -34,7 +85,9 @@ class Segmenter(ProcessorNode):
             - im (np.array): (h, w, 3)
         
         - Returns:
-            - mask: np.array of shape (h, w, num_classes)
+            - masks: np.array of shape (nb_masks, h, w)
+            - classes: np.array of shape (nb_masks,)
+            - scores: np.array of shape (nb_masks,)
         '''
         return self._segment(im)
 
@@ -78,7 +131,6 @@ class TensorflowSegmenter(Segmenter):
                 min_score_threshold = 0.5,
                 nb_tasks = 1,
                 device_type = GPU):
-        self._tensorflow_model = None
         self._num_classes = num_classes
         self._path_to_pb_file = path_to_pb_file
         
@@ -109,6 +161,7 @@ class TensorflowSegmenter(Segmenter):
             remote_url = BASE_URL_SEGMENTATION + self._remote_model_file_name
             self._path_to_pb_file = get_file(self._remote_model_file_name, remote_url)
         
+        '''
         self._tensorflow_model = TensorflowModel(
             self._path_to_pb_file,
             ["image_tensor:0"],
@@ -116,12 +169,15 @@ class TensorflowSegmenter(Segmenter):
                 "num_detections:0", "detection_masks:0"],
             device_id = device_id
         )
+        '''
+        # TODO: Will work on this later
     
     def close(self):
         '''
         Closes tensorflow model session.
         '''
-        self._tensorflow_model._close_session()
+        # TODO: Write code to close model session
+        #self._tensorflow_model._close_session()
     
     def _segment(self, im : np.array) -> np.array:
         '''
@@ -129,10 +185,12 @@ class TensorflowSegmenter(Segmenter):
             - im (np.array): (h, w, 3)
         
         - Returns:
-            - masks: np.array of shape (h, w, num_classes)
-            - classes:
-            - scores:
+            - masks: np.array of shape (nb_masks, h, w)
+            - classes: np.array of shape (nb_masks,)
+            - scores: np.array of shape (nb_masks,)
         '''
+        # TODO: Work on this code later on.
+        # Will be modified very much.
         h, w, _ = im.shape
         im_expanded = np.expand_dims(im, axis = 0)
         boxes, scores, classes, nb_detections, masks = self._tensorflow_model.run_on_input(im_expanded)
@@ -140,13 +198,15 @@ class TensorflowSegmenter(Segmenter):
         
         indexes = np.where(scores > self._min_score_threshold)[0]
         boxes, scores, classes, masks = boxes[indexes], scores[indexes], classes[indexes], masks[indexes]
-        print(boxes)
-        print(scores)
-        print(classes)
-        print(masks)
-        scores, classes = np.expand_dims(scores, axis = 1), np.expand_dims(classes, axis = 1)
-        return np.array(masks), np.array(classes), np.array(scores)
-
-
-
         
+        # 1. Create new masks' array to return
+        new_masks = np.zeros((h, w, boxes.shape[0]), dtype = masks.dtype)
+
+        # 2. Resize each mask to the value that it needs to have
+
+        # 3. Place the mask on its location on the array.
+
+        # 4. Convert masks into 0-1 arrays
+        
+        scores, classes = np.expand_dims(scores, axis = 1), np.expand_dims(classes, axis = 1)
+        return new_masks, np.array(classes), np.array(scores)
