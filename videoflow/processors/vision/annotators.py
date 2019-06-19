7113a9688a61a1e5eda1fa3e1d80a07ef38dd6ae
Jadiel deArmas <jadielam@gmail.com>
2019-06-19 10:20:59 -0700

    Added annotator for segmentation

--- videoflow/processors/vision/annotators.py
+++ videoflow/processors/vision/annotators.py
@@ -123,4 +123,89 @@ class TrackerAnnotator(ImageAnnotator):
             cv2.putText(im, label, (xmin, y_label), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self._text_color, lineType = cv2.LINE_AA)
         return im
 
+class SegmenterAnnotator(ImageAnnotator):
+    '''
+    Draws bounding boxes on images.
+    - Arguments:
+        - class_labels_path: path to pbtxt file that defines the labels indexes
+        - class_labels_dataset: If class_labels_path is None, then we use this attribute to \
+            download the file from the releases folder.  Currently supported datasets \
+            are: ``coco``, ``oidv4``, ``pascal`` and ``kitti``.
+        - transparency: A value between 0 and 1 for the transparency of the mask
+    '''
 
+    supported_datasets = [
+        'coco',
+        'oidv4',
+        'pascal',
+        'kitti'
+    ]
+
+    colors = [
+        (255, 0, 0),
+        (0, 255, 0),
+        (0, 0, 255),
+        (255, 255, 0),
+        (255, 0, 255),
+        (0, 255, 255),
+        (255, 127.5, 0),
+        (255, 0, 127.5),
+        (127.5, 255, 0),
+        (0, 255, 127.5),
+        (127.5, 0, 255),
+        (0, 127.5, 255)
+    ]
+
+    def __init__(self, class_labels_path = None, class_labels_dataset = 'coco', 
+                transparency = 0.5, nb_tasks = 1):
+        
+        if class_labels_path is None and class_labels_dataset is None:
+            raise ValueError('If class_labels_path is None, then class_labels_dataset cannot be None')
+
+        if class_labels_path is None:
+            if class_labels_dataset not in self.supported_datasets:
+                raise ValueError('dataset is not one of supported datasets: {}'.format(', '.join(self.supported_datasets)))
+            labels_file_name = f'labels_{class_labels_dataset}.pbtxt'
+            remote_url = BASE_URL_DETECTION + labels_file_name
+            class_labels_path = get_file(labels_file_name, remote_url)
+
+        self._index_label_d = parse_label_map(class_labels_path)
+        self._transparency = transparency
+        super(SegmenterAnnotator, self).__init__(nb_tasks = nb_tasks)
+
+    def _annotate(self, im : np.array, annotations : list) -> np.array:
+        '''
+        - Arguments:
+            - im: np.array of shape (h, w, 3)
+            - annotations: a list with 3 entries:
+                - masks: np.array of shape (nb_masks, h, w)
+                - classes: np.array of shape (nb_masks, )
+                - scores: np.array of shape (nb_masks, )
+        
+        - Returns:
+            - annotated_im: image with the visual annotations embedded in it.
+        '''
+        masks = annotations[0]
+        classes = annotations[1]
+        
+        to_return = im.copy().astype(float)
+
+        # TODO: Add border to masks
+        # TODO: Think about how to solve issue of overlaping masks
+        # TODO: Add class names to masks
+
+        for idx, mask in enumerate(masks):
+            alpha = cv2.merge((mask, mask, mask))
+            alpha = alpha.astype(float)
+            alpha[alpha == 1.0] = self._transparency
+
+            #1. Mask foreground
+            foreground = np.zeros_like(to_return, dtype = float)
+            foreground[:] = self.colors[int(classes[idx]) % len(self.colors)]
+            foreground = cv2.multiply(alpha, foreground)
+            
+            #2. Image background
+            background = cv2.multiply(1.0 - alpha, to_return)
+            to_return = cv2.add(foreground, background)
+        
+        return to_return.astype(int)
