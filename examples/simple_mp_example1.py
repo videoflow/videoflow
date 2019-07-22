256f107db85145a278e5529ff11785fa68330b48
Jadiel deArmas <jadielam@gmail.com>
2019-07-22 14:52:04 -0400

    Changing reporting of bottlenecks to a time with better statistics

--- examples/simple_mp_example1.py
+++ examples/simple_mp_example1.py
@@ -4,8 +4,8 @@ from videoflow.processors import IdentityProcessor, JoinerProcessor
 from videoflow.consumers import CommandlineConsumer
 
 producer = IntProducer(0, 40, 0.1)
-identity = IdentityProcessor(fps = 2, nb_tasks = 2, name = 'i1')(producer)
-identity1 = IdentityProcessor(fps = 2, nb_tasks = 2, name = 'i2')(identity)
+identity = IdentityProcessor(fps = 2, nb_tasks = 5, name = 'i1')(producer)
+identity1 = IdentityProcessor(fps = 2, nb_tasks = 5, name = 'i2')(identity)
 joined = JoinerProcessor(nb_tasks = 5)(identity, identity1)
 printer = CommandlineConsumer()(joined)
 flow = Flow([producer], [printer])
